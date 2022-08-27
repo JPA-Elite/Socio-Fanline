0cff812cf35bcf7b14656aa9a2430168458a5baf
Joshua Algadipe <105594738+joshxb@users.noreply.github.com>
2022-08-27 16:24:06 +0800

    save

--- PieceWorkerEmployee.java
+++ PieceWorkerEmployee.java
@@ -4,14 +4,13 @@ import static java.lang.System.out;
 public class PieceWorkerEmployee {
     static int empID;
     static String empName;
-    static double totalSales;
-    static String[] empList = { "Mary grace Galan", "Mikey Ferrer",
-            "Jhon Vincent Arnaiz", "Lady ann Lebosada", "Christian Lance" };
-    static double baseSalary;
+    static double PieceChoice;
+    static String[] empList = { "Jei ann Bayer", "Cristopher Lawrence",
+            "Joseph Menoza", "Caspher Mae Ann", "Jaymar Sta.Ana" };
 
-    static int[] idList = { 21123113, 421256743, 15345275, 70245023, 13487565 };
-    static double[] TotalSalesList = { 13499, 42870, 113457, 10700, 5200 };
-
+    static double[] totalPiecesFinished = { 324, 120, 87, 45, 101 };
+    static double ratePerPiece = 0;
+    static int[] idList = { 21026113, 401252713, 05325215, 50246013, 12437515 };
 
     // Getter
     public String getName() {
@@ -34,13 +33,14 @@ public class PieceWorkerEmployee {
         TestingMain();
     }
 
-
     public static void main(String[] args) {
         String input_store;
         Scanner obj = new Scanner(System.in);
         PieceWorkerEmployee main = new PieceWorkerEmployee();
         
-        out.println("\nAdd base salary to the employees: ");
+
+      
+        out.println("Select employee: ");
         while (true) {
             String input = obj.nextLine();
             if (isAnumber(input)) {
@@ -53,39 +53,35 @@ public class PieceWorkerEmployee {
             }
         }
 
-        baseSalary = Double.parseDouble(input_store);
-      
-        out.println("Select employee: ");
-        String input2 = obj.nextLine();
         
-        main.setName(input2);
+        main.setName(input_store);
 
         if (Integer.valueOf(main.getName()) <= empList.length) {
             switch(Integer.valueOf(main.getName())){
                 case 1:
                     empName = empList[0];
                     empID = idList[0];
-                    totalSales = TotalSalesList[0];
+                    PieceChoice = totalPiecesFinished[0];
                     break;
                 case 2:
                     empName = empList[1];
                     empID = idList[1];
-                    totalSales = TotalSalesList[1];
+                    PieceChoice = totalPiecesFinished[1];
                     break;
                 case 3:
                     empName = empList[2];
                     empID = idList[2];
-                    totalSales = TotalSalesList[2];
+                    PieceChoice = totalPiecesFinished[2];
                     break;
                 case 4:
                     empName = empList[3];
                     empID = idList[3];
-                    totalSales = TotalSalesList[3];
+                    PieceChoice = totalPiecesFinished[3];
                     break;
                 case 5:
                     empName = empList[4];
                     empID = idList[4];
-                    totalSales = TotalSalesList[4];
+                    PieceChoice = totalPiecesFinished[4];
                     break;
                 default:
                     break;
@@ -94,50 +90,70 @@ public class PieceWorkerEmployee {
             out.println("Invalid type occurred!");
         }
 
-        displayInfo(empID, empName, totalSales, baseSalary);
-        out.println("Click \'okay\' to show salary + the base salary.");
-        String input3 = obj.nextLine();
-        String compute = (input3.equals("okay")) ? computeSalary(totalSales, baseSalary)
-                    : "Invalid type occurred";
-        out.println("Total salary: Php." + compute);
-
+        displayInfo(empID, empName, PieceChoice);
+        out.println("Click \'okay\' to show salary + the rate per piece.\n(ex: okay 100)");
+        while (true){
+            try {
+                String[] input3 = obj.nextLine().split(" ");
+                if (input3.length >= 3) {
+                    out.println("Format is invalid!");
+                }
+                else {
+    
+                    if (input3[0].equals("okay") && isAnumber(input3[1])) {
+                        ratePerPiece = Double.parseDouble(input3[1]);
+                        String computed = computeSalary(PieceChoice, ratePerPiece);
+                        out.println("Total salary: Php." + computed);
+                        break;
+                    }
+                    else {
+                        out.println("Invalid type occurred");
+                    }
         
     
-      
-
+                    
+                }
+            }
+            catch (Exception e) {
+                out.println("Format is invalid!");
+            }
+            
+            
+        }
         
 
     }
 
-    static void displayInfo(int empID, String empName, double totalSales, double baseSalary) {
+    static void displayInfo(int empID, String empName, double PieceChoice) {
         out.println("Employee Name: " + empName);
         out.println("Employee ID: " + empID);
-        out.println("Total Sales: Php." + totalSales);
-        out.println("Base salary: Php." + baseSalary);
-     
+        out.println("Total Piece Finished: " + Math.round(PieceChoice) + "pc/s");
 
     }
 
-    static String computeSalary(double totalSales, double baseSalary) {
-        double result = 0;
-        if (totalSales <= 10000){
-            result = (totalSales * 0.05) + baseSalary;
+    static String computeSalary(double PieceChoice, double ratePerPiece) {
+        double result;
+        double store;
+        double store2;
+        store2 = PieceChoice * ratePerPiece;
+        store = PieceChoice;
+
+        while (true) {
+            if ((store - 100) < 100) {
+                break;
+            }
+            store2 = store2 * 10;
+            store = store - 100;
         }
-        else if (totalSales > 10000 && totalSales < 50000){
-            result = (totalSales * 0.10) + baseSalary;
-        }
-        else if (totalSales > 50000 && totalSales < 100000){
-            result = (totalSales * 0.15) + baseSalary;
-        }
-        else if (totalSales > 100000){
-            result = (totalSales * 0.30) + baseSalary;
-        }
+
+        result = store2;
+
         return String.valueOf(result);
+
     }
 
+    private static boolean isAnumber(String text) {
 
-    private static boolean isAnumber(String text){
-        
         if (text.equals("") || text == null) {
             return false;
         }
@@ -145,8 +161,7 @@ public class PieceWorkerEmployee {
         try {
             Double.parseDouble(text);
             return true;
-        }
-        catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             e.getMessage();
             return false;
         }

d002882f4fc6865e29793dd8c9dac83f64e39b5f
joshxb <21103808@usc.edu.ph>
2022-08-30 18:50:11 +0800

    'save

--- EmployeeRoster.java
+++ EmployeeRoster.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+
 import static java.lang.System.out;
 import java.util.Scanner;
 import java.util.Formatter;
+import java.util.Random;
 
 public class EmployeeRoster extends Employee {
     // static String[] EmployeeGroupList1 = { };
@@ -21,6 +24,9 @@ public class EmployeeRoster extends Employee {
     static ArrayList<String> storeTypesEmployees = new ArrayList<String>();
     String list_types;
     static Formatter form = new Formatter();
+    static Random rand = new Random();
+    static EmployeeRoster main = new EmployeeRoster();
+    static Scanner in = new Scanner(System.in);
 
     // Constructor
     public EmployeeRoster() {
@@ -43,17 +49,13 @@ public class EmployeeRoster extends Employee {
     }
 
     public static void main(String[] args) {
-
-        EmployeeRoster main = new EmployeeRoster();
-        Scanner in = new Scanner(System.in);
-
         Group1List();
         Group2List();
         Group3List();
         Group4List();
         out.println(String.format("Types of Employees:\n%s\nSelect option: ", main.list_types));
         String inputted = in.nextLine();
-        in.close();
+
         if (isAnumber(inputted)) {
             switch (Integer.parseInt(inputted)) {
                 case 1:
@@ -86,7 +88,24 @@ public class EmployeeRoster extends Employee {
                     displayEmployee();
                     break;
                 case 6:
-                    
+                    out.println("1. Add Employee\n2.Remove employee");
+                    String inputted2 = in.nextLine();
+                    if (isAnumber(inputted2)) {
+                        switch (Integer.parseInt(inputted2)) {
+                            case 1:
+                                typeofTransaction("+1");
+                                break;
+                            case 2:
+                                typeofTransaction("-1");
+                                break;
+                            default:
+                                break;
+                        }
+                    } else {
+                        out.println("Invalid type occurred!");
+                    }
+
+                    break;
 
                 default:
                     out.println("Incorrect number selected!");
@@ -159,25 +178,25 @@ public class EmployeeRoster extends Employee {
 
     public static void displayHourlyEmployee() {
         for (int i = 0; i < storeGroup1.size(); i++) {
-            out.println(storeGroup1.get(i));
+            out.println((i + 1) + ". " + storeGroup1.get(i));
         }
     }
 
     public static void displayPeiceWorkerEmployee() {
         for (int i = 0; i < storeGroup2.size(); i++) {
-            out.println(storeGroup2.get(i));
+            out.println((i + 1) + ". " + storeGroup2.get(i));
         }
     }
 
     public static void displayCommissionEmployee() {
         for (int i = 0; i < storeGroup3.size(); i++) {
-            out.println(storeGroup3.get(i));
+            out.println((i + 1) + ". " + storeGroup3.get(i));
         }
     }
 
     public static void displayBasePlusCommissionEmployee() {
         for (int i = 0; i < storeGroup4.size(); i++) {
-            out.println(storeGroup4.get(i));
+            out.println((i + 1) + ". " + storeGroup4.get(i));
         }
     }
 
@@ -201,8 +220,129 @@ public class EmployeeRoster extends Employee {
 
     }
 
-    public static Object addEmployee(int empID, String empName) {
-        return "wdwd";
+    public static Object addEmployee(int empID, String empName, String empType) {
+        return empID + " " + empName + " " + empType;
+    }
+
+    public static Object computeTransaction(int trans) {
+        int emptype = 0;
+        switch (trans) {
+            case 1:
+                displayHourlyEmployee();
+                emptype = countHourlyEmployee();
+                break;
+            case 2:
+                displayCommissionEmployee();
+                emptype = countCommissionEmployee();
+                break;
+            case 3:
+                displayBasePlusCommissionEmployee();
+                emptype = countBasePlusCommissionEmployee();
+                break;
+            case 4:
+                displayPeiceWorkerEmployee();
+                emptype = countPeiceWorkerEmployee();
+                break;
+
+            default:
+                break;
+        }
+
+        out.println("\nSelect one: ");
+        String inputted5 = in.nextLine();
+
+        if (isAnumber(inputted5) && (Integer.parseInt(inputted5) <= emptype)) {
+            int removeIndex = Integer.parseInt(inputted5);
+            for (int i = removeIndex; i < (emptype - 1); i++) {
+                if (trans == 1) {
+                    G1.empList[i] = G1.empList[i + 1];
+
+                } else if (trans == 2) {
+                    G2.empList[i] = G2.empList[i + 1];
+
+                } else if (trans == 2) {
+                    G3.empList[i] = G3.empList[i + 1];
+
+                } else if (trans == 2) {
+                    G4.empList[i] = G4.empList[i + 1];
+
+                }
+
+            }
+            displayHourlyEmployee();
+
+        } else {
+            out.println("Invalid type occurred!");
+        }
+        return 0;
+    }
+
+    public static void typeofTransaction(String type_transc) {
+        String store = "";
+        store += "\n1. ".concat(G1.name).concat("\n");
+        store += "2. ".concat(G2.name).concat("\n");
+        store += "3. ".concat(G3.name).concat("\n");
+        store += "4. ".concat(G4.name).concat("\n");
+        if (type_transc.equals("+1")) {
+
+            String empName = "";
+            int randomNum = rand.nextInt(99999999);
+            String empType = "";
+            out.println("Name of the employee: ");
+            String inputted3 = in.nextLine();
+            empName = inputted3;
+            out.println(String.format("%s", store));
+            out.println("Choose type: ");
+            String inputted4 = in.nextLine();
+            if (isAnumber(inputted4)) {
+                switch (Integer.parseInt(inputted4)) {
+                    case 1:
+                        empType = G1.name;
+                        break;
+                    case 2:
+                        empType = G2.name;
+                        break;
+                    case 3:
+                        empType = G3.name;
+                        break;
+                    case 4:
+                        empType = G4.name;
+                        break;
+
+                    default:
+                        break;
+                }
+            } else {
+                out.println("Invalid type occurred!");
+            }
+            out.println("The new Id of the employee is " + randomNum);
+
+            out.println("result:\n" + addEmployee(randomNum, empName, empType));
+        } else if (type_transc.equals("-1")) {
+            out.println(String.format("%s", store));
+            out.println("Choose type: ");
+            String inputted4 = in.nextLine();
+            if (isAnumber(inputted4)) {
+                switch (Integer.parseInt(inputted4)) {
+                    case 1:
+                        computeTransaction(1);
+                        break;
+                    case 2:
+                        computeTransaction(2);
+                        break;
+                    case 3:
+                        computeTransaction(3);
+                        break;
+                    case 4:
+                        computeTransaction(4);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
     }
 
 }
--- Parse.java
+++ Parse.java
@@ -1,5 +1,7 @@
 import static java.lang.System.out;
 
+import java.util.Arrays;
+
 // class Hello {
 //     String a = "dfef";
 
@@ -8,21 +10,31 @@ import static java.lang.System.out;
 //     }
 // }
 
-public class Parse extends Employee{
+public class Parse extends Employee {
 
     static Employee main = new Employee();
 
     static Employee.FirstGroup main2 = main.new FirstGroup();
+
     public static void main(String[] args) {
         // Hello outer = new Hello();
 
         // Hello.ahem inner = outer.new ahem();
         // out.println(inner.b);
 
+        int[] my_array = { 25, 14, 56, 15, 36, 56, 77, 18, 29, 49 };
 
-       
-        out.println(main2.empList[1]);
+        System.out.println("Original Array : " + Arrays.toString(my_array));
 
+        // Remove the second element (index->1, value->14) of the array
+        int removeIndex = 1;
+
+        for (int i = removeIndex; i < my_array.length - 1; i++) {
+            my_array[i] = my_array[i + 1];
+        }
+        // We cannot alter the size of an array , after the removal, the last and second
+        // last element in the array will exist twice
+        System.out.println("After removing the second element: " + Arrays.toString(my_array));
 
     }
 
